0630f2c800f093fcc293cf4779a2daf9832a8bca
mon <monmon.plaza@gmail.com>
2020-01-12 21:47:26 +0800

    Updates

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".testimonial-slider").slick({
+    arrows: true,
+    dots: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>'
+  });
+});
